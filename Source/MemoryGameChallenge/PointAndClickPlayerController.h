3d83de5b6a7f982a5665d19ffaf0d7d1f65fc098
tompsognathus <tompsognathus@gmail.com>
2023-06-14 10:32:33 +0100

    added basic game board and set up player controller for mouse controlled clicks

---
+++ Source/MemoryGameChallenge/PointAndClickPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PointAndClickPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEMORYGAMECHALLENGE_API APointAndClickPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
